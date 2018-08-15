13af1180b2136f1fc1da10579ecde2bcb4503ad3
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-15 17:17:01 -0600

    Updates routes to nest food with meals

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :foods, except: [:new, :edit]
-      resources :meals, only: [:index]
-      get '/meals/:id/foods', to: 'meals#show'
-      post 'meals/:meal_id/foods/:id', to: 'meals#create'
-      delete 'meals/:meal_id/foods/:id', to: 'meals#destroy'
+      resources :meals, except: [:new, :edit] do
+        resources :foods, except: [:new, :edit]
+        # get '/meals/:id/foods', to: 'meals#show'
+        # post 'meals/:meal_id/foods/:id', to: 'meals#create'
+        # delete 'meals/:meal_id/foods/:id', to: 'meals#destroy'
+      end
     end
   end
 end
